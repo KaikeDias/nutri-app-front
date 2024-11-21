8eb5f3ff666431b231772f18e387d14014b7a3eb
Kaike <kaikedias98@gmail.com>
2024-11-21 10:32:22 -0300

    feat: add routes for login,register and home

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,12 +3,21 @@ import { RouteRecordRaw } from 'vue-router'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Login.vue') }, 
+      { path: 'login', component: () => import('pages/Login.vue') },
+      { path: 'register', component: () => import('pages/Register.vue') },
+    ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '/home', component: () => import('pages/Home.vue') }],
+    meta: {
+      requiresAuth: true
+    }
+  },
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
