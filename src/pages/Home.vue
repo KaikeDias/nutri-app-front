db7c41ba420d67e5a5bc65bc6a4b496d1d398015
Kaike <kaikedias98@gmail.com>
2024-11-21 10:30:42 -0300

    feat: add home

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <div v-if="user">
+      <p>
+        Hello {{ user.user_metadata.name }}
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+  import useUserAuth from 'src/composables/useAuthUser';
+  import { ref } from 'vue'
+
+  defineOptions({
+    name: 'Home'
+  })
+
+  const {user} = useUserAuth()
+
+</script>
