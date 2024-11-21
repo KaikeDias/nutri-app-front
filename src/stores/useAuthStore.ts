cbeee2ca3477f2c1db1e2b675de410939338d095
Kaike <kaikedias98@gmail.com>
2024-11-21 10:57:09 -0300

    feat: add auth store

---
+++ src/stores/useAuthStore.ts
@@ -0,0 +1,18 @@
+import { User } from '@supabase/supabase-js';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref<User | null>(null);
+  const loading = ref(false);
+
+  const setUser = (newUser: any) => {
+    user.value = newUser;
+  };
+
+  const setLoading = (isLoading: boolean) => {
+    loading.value = isLoading;
+  };
+
+  return { user, loading, setUser, setLoading };
+});
