93fb67c6052abcff037820be91bd2ab9b9935c69
Kaike <kaikedias98@gmail.com>
2024-11-21 10:29:17 -0300

    feat: add state change

--- src/boot/supabase.ts
+++ src/boot/supabase.ts
@@ -1,10 +1,18 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import useUserAuth from 'src/composables/useAuthUser';
 
 const supabaseUrl: string = '';
 const supabaseAnonKey: string = '';
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey)
 
-console.log('supabase: ', supabase)
+
+
+supabase.auth.onAuthStateChange(async (event, session) => {
+    const {user} = useUserAuth()
+    
+    user.value = session?.user || null
+    console.log(user.value)
+});
 
 export default function useSupabase() {
     return {supabase}
