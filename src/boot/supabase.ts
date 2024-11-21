cea36c38ac9045abee0a97f82b33f65e004c8414
Kaike <kaikedias98@gmail.com>
2024-11-21 10:57:26 -0300

    feat: add loading state

--- src/boot/supabase.ts
+++ src/boot/supabase.ts
@@ -1,19 +1,22 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 import useUserAuth from 'src/composables/useAuthUser';
+import { useAuthStore } from 'src/stores/useAuthStore';
 
-const supabaseUrl: string = '';
-const supabaseAnonKey: string = '';
+const supabaseUrl: string = 'https://mftofuwjfgxndyacyqcn.supabase.co';
+const supabaseAnonKey: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mdG9mdXdqZmd4bmR5YWN5cWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAxNDIzMzksImV4cCI6MjA0NTcxODMzOX0.jOWqMT2KTUdZl0yx1jDL4FPK0UX-gF0tMQSq-RHhrmc';
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey)
 
-
-
 supabase.auth.onAuthStateChange(async (event, session) => {
-    const {user} = useUserAuth()
-    
-    user.value = session?.user || null
-    console.log(user.value)
-});
-
+    const authStore = useAuthStore();
+    authStore.setLoading(true);
+  
+    try {
+      const user = session?.user || null;
+      authStore.setUser(user);
+    } finally {
+      authStore.setLoading(false);
+    }
+  });
 export default function useSupabase() {
     return {supabase}
 }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,25 @@
 <template>
   <q-page class="flex flex-center">
-    <div v-if="user">
-      <p>
-        Hello {{ user.user_metadata.name }}
-      </p>
+    <div v-if="loading">
+      <q-spinner size="40px" color="primary" />
+    </div>
+    <div v-else-if="user">
+      <p>Hello {{ user.user_metadata.name }}</p>
+    </div>
+    <div v-else>
+      <p>Please log in.</p>
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-  import useUserAuth from 'src/composables/useAuthUser';
-  import { ref } from 'vue'
+  import { storeToRefs } from 'pinia';
+import { useAuthStore } from 'src/stores/useAuthStore';
 
   defineOptions({
-    name: 'Home'
-  })
+    name: 'Home',
+  });
 
-  const {user} = useUserAuth()
-
+  const authStore = useAuthStore();
+  const { user, loading } = storeToRefs(authStore);
 </script>
